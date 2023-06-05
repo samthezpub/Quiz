4c3919645b373c61a94536d6eef3dd98a9f31786
samtheman <lil.rhymes9@gmail.com>
2023-06-05 19:46:43 +0300

    Table from file

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -1,2 +1,37 @@
 <h1>Quiz'es</h1>
 <a href="{{route('create')}}">Create Quiz</a>
+
+@php
+    function DoListFromFile()
+        {
+            if (Storage::fileExists('local')) {
+                $arr = explode(PHP_EOL, Storage::get('local'));
+                return $arr; 
+            }
+            return 0;
+        }
+        $arr = DoListFromFile();
+    
+@endphp
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+    </tr>
+    
+        
+        
+        
+            @for($i = 0; $i < count($arr)-1; $i++)
+            {
+                <tr>
+                    <td>{{$i}}</td>
+                    <td>{{$arr[$i]}}</td>
+                </tr>
+            }
+            @endfor
+        
+        
+        
+</table>
